215d93f0f5f09a5b6fed885d43a787c5f2352b0c
Gustavo <gustax.dev@gmail.com>
2025-11-02 00:06:43 -0300

    Adding Gateway and Account projects

---
+++ src/Account/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Kairos.Account;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAccount(this IServiceCollection services)
+    {
+        // services.AddScoped<IAccountService, AccountService>();
+        return services;
+    }
+}
